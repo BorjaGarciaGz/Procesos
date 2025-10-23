aaa7e32987a46e4a43d977254a24ee248f53df68
BorjaGarciaGz <3167254@alu.murciaeduca.es>
2025-10-23 09:58:51 +0200

    Actualizado

--- TareaPSPCorregida/src/Ejercicio5/App2.java
+++ TareaPSPCorregida/src/Ejercicio5/App2.java
@@ -5,6 +5,23 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * Crea dos aplicaciones
+ * a. Aplicación 1
+ * i. Le va pidiendo números enteros al usuario hasta que este le introduzca un 0 o un
+ * número negativo.
+ * ii. Todos estos números se los enviará a una Aplicación 2 que le deberá una serie de
+ * información que deberá mostrar por terminal. Estos datos se enviarán para que
+ * Aplicación 2 los reciba por su entrada estándar.
+ * b. Aplicación 2
+ * i. Recibirá un número indeterminado de números mayores a 0 y deberá imprimir por
+ * consola los siguientes resultados
+ * 1. Dos números mayores
+ * 2. Menor número
+ * 3. Media
+ * 4. Número más repetido.
+ */
+
 public class App2 {
 
     public static void main(String[] args) {
