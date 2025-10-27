9d0e08bce4da0d9a4406798ac2749061c617d809
BorjaGarciaGz <3167254@alu.murciaeduca.es>
2025-10-27 11:53:13 +0100

    Mas

---
+++ Practica2_1/src/main/java/org/example/practica2_1/MainLayouts.java
@@ -0,0 +1,19 @@
+package org.example.practica2_1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainLayouts extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainLayouts.class.getResource("MainLayouts.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
